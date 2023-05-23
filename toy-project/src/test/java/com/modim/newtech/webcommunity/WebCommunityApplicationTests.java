a39e4d5ffd35f16dcba0834315b4c777a4aa4750
alice <alice@modim.co.kr>
2023-05-23 17:09:05 +0900

    first commit

---
+++ toy-project/src/test/java/com/modim/newtech/webcommunity/WebCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modim.newtech.webcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
