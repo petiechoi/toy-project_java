c1be08e16093f956355f0e717431d7de87ff0508
HoonBear <dev.jh.son@gmail.com>
2023-06-12 16:06:47 +0900

    Fix: 테스트 안되는거 수정

--- toy-project/src/test/java/com/modim/newtech/toyproject/controller/MemberControllerTest.java
+++ toy-project/src/test/java/com/modim/newtech/toyproject/controller/MemberControllerTest.java
@@ -1,25 +1,36 @@
 package com.modim.newtech.toyproject.controller;
 
-import com.modim.newtech.toyproject.community.member.controller.MemberRestController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
-@WebMvcTest(controllers = MemberRestController.class)
+@ExtendWith({SpringExtension.class})
+@SpringBootTest
+@ActiveProfiles("local")
 public class MemberControllerTest {
-    @Autowired
-    private MockMvc mvc;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                                      .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                                      .build();
+    }
 
     @Test
     public void hi리턴한다() throws Exception{
         String ret = "hi";
-        mvc.perform(
-                        MockMvcRequestBuilders.get("/hi"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/hi"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(ret));
     }
